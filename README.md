f9a8e20fc820baa2e931a563a27423b27f17e2c3
Gustavo de Oliveira <tav0dev1337+gh@gmail.com>
2026-01-19 15:24:51 -0300

    feat: first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# consulting-outpost-group
+
+Implementação da https://consulting.outpostgroup.com.br
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Seja bem-vindo a Outpost Consulting!</h1>;
+}
+
+export default Home;
